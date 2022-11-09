eb8527f9377e069b51355f9ad9bb95f2776f2e5b
Rotimi Ishola <74196357+Timiphil@users.noreply.github.com>
2022-11-09 11:07:44 +0100

    update the btn section on js set the animation

--- app.js
+++ app.js
@@ -13,6 +13,25 @@ function pageTransitions() {
          this.className += ' active-btn';
       })
    }
+   // section active class
+   allSections.addEventListener('click', (e) => {
+      const id = e.target.dataset.id;
+      if (id) {
+         // remove selected from the other btns
+         sectBtns.forEach((btn) => {
+            btn.classList.remove('active')
+         })
+         e.target.classList.add('active')
+
+         // hide other sections
+         sections.forEach((section) => { 
+            section.classList.remove('active')
+         })
+
+         const element = document.getElementById(id);
+         element.classList.add('active');
+      }     
+   })
 }
 
 pageTransitions();
