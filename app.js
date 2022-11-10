59522263db603caa4fff9ec3e0cbdf878ac8e980
Rotimi Ishola <74196357+Timiphil@users.noreply.github.com>
2022-11-10 07:45:45 +0100

    updated the header and finish the front page

--- app.js
+++ app.js
@@ -10,26 +10,26 @@ function pageTransitions() {
       sectBtn[i].addEventListener('click', function(){
          let currentBtn = document.querySelectorAll('.active-btn');
          currentBtn[0].className = currentBtn[0].className.replace('active-btn', '');
-         this.className += ' active-btn';
+         this.className += " active-btn";
       })
    }
    // section active class
-   allSections.addEventListener('click', (e) => {
+   allSections.addEventListener("click", (e) => {
       const id = e.target.dataset.id;
       if (id) {
          // remove selected from the other btns
          sectBtns.forEach((btn) => {
-            btn.classList.remove('active')
+            btn.classList.remove("active")
          })
-         e.target.classList.add('active')
+         e.target.classList.add("active")
 
          // hide other sections
          sections.forEach((section) => { 
-            section.classList.remove('active')
+            section.classList.remove("active")
          })
 
          const element = document.getElementById(id);
-         element.classList.add('active');
+         element.classList.add("active");
       }     
    })
 }
